bbbed6802edff4d230a422f4cc5ecc2fb2592d6c
Nicholas montes da silva <nicholasmontes0@gmail.com>
2023-08-20 21:37:20 -0300

    Restructuring with order and order item templates

---
+++ Models/OrderItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Assistant.Models
+{
+    public class OrderItem
+    {
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Value { get; set; }
+    }
+}
